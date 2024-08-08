b7cfe01b1644d82e6c774e0f118b7f2bbad83490
LinkShake <nicolacampo19@gmail.com>
2024-08-08 15:38:54 +0200

    Fixed bug in edit todo route

--- schema/schema.go
+++ schema/schema.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	gorm.Model
 	ID uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Username string
+	Email string
 	Pwd string
 	Todos []Todo `gorm:"foreignKey:UserId"`
 }
